e405b9e3c8d8217859d5a42135f0a938ef3ae0e7
Lucas Cristi <lucasccristi@gmail.com>
2022-10-24 00:27:03 -0300

    Add infos

--- assets/js/cardsExperience.js
+++ assets/js/cardsExperience.js
@@ -1,88 +1,45 @@
-let button = document.getElementById('read-button')
-// console.log(button)
+// let button = document.getElementById('read-button')
+
+// button.addEventListener('click', function () {
+//     let card = document.querySelector('.experience-card')
+
+//     card.classList.toggle('active')
+
+//     if(card.classList.contains('active')){
+//         return button.textContent = 'Ver menos'
+//     }
+
+//     button.textContent = 'Ver mais'
+// })
 
 
-button.addEventListener('click', function () {
-    let card = document.querySelector('.experience-card')
-
-    card.classList.toggle('active')
-
-    if(card.classList.contains('active')){
-        return button.textContent = 'Ver menos'
-    }
-
-    button.textContent = 'Ver mais'
-})
+let button = document.querySelectorAll('.read-button')
 
 
-let button2 = document.getElementById('read-button2')
-// console.log(button)
+for (i = 0; i <= button.length; i++) {
+    button[i].addEventListener('click', event => {
 
 
-button2.addEventListener('click', function () {
-    let card = document.querySelector('.experience-card2')
+        let card = document.querySelectorAll('.experience-card')
+        console.log(card)
+        card.forEach(array => {
+            console.log(card)
+            let i = 0
+            array
+            i++
+            const toggleCard = array.classList.toggle('active')
+            console.log(array)
+            const eventButton = event.target
 
-    card.classList.toggle('active')
-
-    if(card.classList.contains('active')){
-        return button2.textContent = 'Ver menos'
-    }
-
-    button2.textContent = 'Ver mais'
-})
-
-
-let button3 = document.getElementById('read-button3')
-// console.log(button)
-
-
-button3.addEventListener('click', function () {
-    let card = document.querySelector('.experience-card3')
-
-    card.classList.toggle('active')
-
-    if(card.classList.contains('active')){
-        return button3.textContent = 'Ver menos'
-    }
-
-    button3.textContent = 'Ver mais'
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let button = document.getElementsByClassName('section-experiences')
-// const itensButton = button[0].children
-// const card = document.querySelectorAll('.experience-card')
-// const itensCard = card.length
-
-
-// for (i = 1; i < itensButton.length; i++) {
-// itensButton[i].addEventListener('click', evento => {
-//         const clickButton = evento.target
-//         for (i = 0; i < card.length; i++) {
-//         // console.log(clickButton)
-
-//             card[i].classList.toggle('active')
-//             if (card[i].classList.contains('active')) {
-//                 return clickButton.textContent = 'Ver menos'
-//             }
-//             clickButton.textContent = 'Ver mais'
-//         }
-//     })
-// }
+            if (toggleCard) {
+                return eventButton.textContent = 'Ver menos'   
+            }
+             return eventButton.textContent = 'Ver mais'
+        }
+        )
+    }  
+    )
+}
 
 
 
